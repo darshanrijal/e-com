02fae220f692a63c70dae98d8a5bb6f0bc7695ea
Darshan rijal <darshanrijal0@gmail.com>
2024-11-06 14:41:33 +0545

    setup wix oauth and wix client

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
