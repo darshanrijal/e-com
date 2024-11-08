58961ede06af801148eb2d4ccc734a1b34fb16bf
Darshan rijal <darshanrijal0@gmail.com>
2024-11-08 14:47:20 +0545

    fix cls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Navbar />
-            {children}
+            <div className="min-h-[50vh]">{children}</div>
             <Footer />
             <Toaster />
           </ThemeProvider>
